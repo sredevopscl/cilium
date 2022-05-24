bab593267731cf47652d5aac413d9592ee35cbc7
Gilberto Bertin <jibi@cilium.io>
2022-05-24 12:00:50 +0200

    ipsec: rename ipSecKeysGlobalLock to ipSecLock
    as now this lock is used to protect also ipSecCurrentKeySPI in addition
    to ipSecKeysGlobal.
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -69,7 +69,7 @@ type ipSecKey struct {
 }
 
 var (
-	ipSecKeysGlobalLock lock.RWMutex
+	ipSecLock lock.RWMutex
 
 	// ipSecKeysGlobal can be accessed by multiple subsystems concurrently,
 	// so it should be accessed only through the getIPSecKeys and
@@ -81,8 +81,8 @@ var (
 )
 
 func getIPSecKeys(ip net.IP) *ipSecKey {
-	ipSecKeysGlobalLock.RLock()
-	defer ipSecKeysGlobalLock.RUnlock()
+	ipSecLock.RLock()
+	defer ipSecLock.RUnlock()
 
 	key, scoped := ipSecKeysGlobal[ip.String()]
 	if scoped == false {
@@ -569,8 +569,8 @@ func loadIPSecKeys(r io.Reader) (int, uint8, error) {
 	var keyLen int
 	scopedLog := log
 
-	ipSecKeysGlobalLock.Lock()
-	defer ipSecKeysGlobalLock.Unlock()
+	ipSecLock.Lock()
+	defer ipSecLock.Unlock()
 
 	if err := encrypt.MapCreate(); err != nil {
 		return 0, 0, fmt.Errorf("Encrypt map create failed: %v", err)
@@ -746,8 +746,8 @@ func StartKeyfileWatcher(ctx context.Context, keyfilePath string, nodediscovery 
 }
 
 func doReclaimStaleKeys() {
-	ipSecKeysGlobalLock.Lock()
-	defer ipSecKeysGlobalLock.Unlock()
+	ipSecLock.Lock()
+	defer ipSecLock.Unlock()
 
 	xfrmStateList, err := netlink.XfrmStateList(0)
 	if err != nil {
